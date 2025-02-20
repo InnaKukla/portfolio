0649881c00f9cbc53f4d097f633de0507ada01d3
Inna Kukla <0969125133@ukr.net>
2025-02-21 01:27:47 +0200

    Initial commit with built files

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
-    export default defineConfig({
-      base: '/portfolio/',
-      plugins: [
-        react({
-          babel: {
-            plugins: [
-              [
-                'babel-plugin-styled-components',
-                {
-                  displayName: true,
-                  fileName: false
-                }
-              ]
-            ]
-          }
-        }), svgr()
-      ]
-    })
+export default defineConfig({
+  base: '/portfolio/',
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            'babel-plugin-styled-components',
+            {
+              displayName: true,
+              fileName: false
+            }
+          ]
+        ]
+      }
+    }), 
+    svgr()
+  ],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html',
+      output: {
+        entryFileNames: 'main.js',
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom']
+  }    
+});
